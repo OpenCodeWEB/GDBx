5ca4d675b3927f36648636647ff5169a05ba2d12
ABsUP <ABsUP@users.noreply.github.com>
2026-08-25 00:12:52 +0600

    chore: gitignore generated _gun_child_test.mjs (spawned by gunx client test)

--- _gun_child_test.mjs
+++
@@ -1,8 +0,0 @@
-import Gun from "gun";
-const g = new Gun({"peers":["https://gdbx.xup.workers.dev/gunx"],"radisk":false,"localStorage":false,"axe":false,"multicast":false});
-setTimeout(() => {
-  g.get("gdbx-client-test/1787595074046-live").on((d) => {
-    if (d && d.message) { console.log("CHILD-GOT:" + d.message); process.exit(0); }
-  });
-}, 2500);
-setTimeout(() => process.exit(2), 35000);
